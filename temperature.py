30efddf9db43f189913de23ceca92e0a9f51119e
Michael Ludvig <mludvig@logix.net.nz>
2019-10-19 18:38:42 +1300

    Support aliases

--- temperature.py
+++ temperature.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+import json
 import ow
 
 class Temperature:
@@ -8,23 +9,29 @@ class Temperature:
         # For example DS18B20 connected directly or over DS2482 I2C-to-1wire bridge
         assert(config['mode'] == 'owserver')
         
+        # Load the probe aliases
+        aliases = config.get('aliases', '{}')
+        self.aliases = json.loads(aliases)
+
+        # Connect to the server and list probes
         ow.init(config['server'])
         self.sensors = ow.Sensor('/').sensorList()
-        self.addrs = {}
         for sensor in self.sensors:
-            print('Address: ' + sensor.address)
-            print('Family:  ' + sensor.family)
-            print('ID:      ' + sensor.id)
-            print('Type:    ' + sensor.type)
-            print('Temper:  ' + sensor.temperature)
+            print('Address:   ' + sensor.address)
+            if sensor.address in self.aliases:
+                print('Alias:     ' + self.aliases[sensor.address])
+            print('Family:    ' + sensor.family)
+            print('ID:        ' + sensor.id)
+            print('Type:      ' + sensor.type)
+            print('Curr Temp: ' + sensor.temperature)
             print(' ')
-            self.addrs[sensor.address] = sensor
-
-    def read(self, addr):
-        return self.addrs[addr].temperature
 
     def read_all(self):
         ret = {}
         for sensor in self.sensors:
-            ret[sensor.address] = float(sensor.temperature)
+            if sensor.address in self.aliases:
+                sensor_id = self.aliases[sensor.address]
+            else:
+                sensor_id = sensor.address
+            ret[sensor_id] = float(sensor.temperature)
         return ret
